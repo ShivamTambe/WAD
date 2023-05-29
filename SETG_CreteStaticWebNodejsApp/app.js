7459ee58d9b43c98e91b77322b68c496a8323562
unknown <shivamstambe20222@gmail.com>
2023-05-29 19:15:31 +0530

    done

---
+++ SETG_CreteStaticWebNodejsApp/app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.sendFile('home.html',{root:__dirname});
+});
+
+app.listen(3000, () => {
+    console.log('App listening on port 3000!');
+});
